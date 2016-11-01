795e82444621cb782c16a015938804204dd08fd8
thuhale <thuhale@stat.wisc.edu>
2016-11-01 17:11:24 -0500

    Update recursion

--- graph_check.R
+++ graph_check.R
@@ -38,8 +38,66 @@ E(g)$color[E(mst)$id] <- "yellow"
 plot(g, edge.width = E(g)$weight*20)
 
 par(mfrow = c(1,2))
-plot(sparseAHC(as(A,"sparseMatrix"), linkage = "single"), labels = colnames(A))
-plot(sparseAHC(as(L,"sparseMatrix"), linkage = "single"), labels = colnames(L))
+plot(sparseAHC(as(A,"sparseMatrix"), linkage = "single"), labels = colnames(A), main = "AHC on A")
+plot(sparseAHC(as(L,"sparseMatrix"), linkage = "single"), labels = colnames(L), main = "AHC on L")
+
+
+#Search the tree
+
+
+slideTree = function(g){
+	tau = 1/length(V(g))
+	best = 1000
+	to_cut = 1
+	E(g)$cut = rep(best, length(E(g)))
+	for(i in 1:length(E(g))){
+		g1 = g
+		g1 = delete_edges(g1, E(g1)[i])
+		
+		comp = components(g1)$csize
+		cut = E(g)$weight[i]/((comp[1]+tau)*(comp[2]+tau))
+				
+		if(cut<best){
+			best = cut
+			to_cut = i
+		}
+		E(g)$cut[i] = cut
+	}
+	g1 = g
+	g1 = delete_edges(g1, E(g1)[to_cut])
+
+	mem = components(g1)$membership
+	left_tree = induced_subgraph(g, V(g)[mem==1])
+	right_tree = induced_subgraph(g, V(g)[mem==2])
+	print(V(left_tree))
+	print(V(right_tree))
+	return(list(left_tree, right_tree))
+}
+
+
+x = slideTree(mst)
+left_tree = x[[1]]
+right_tree = x[[2]]
+
+cutTree = function(g, dt){
+	if(length(V(g)) == 1){
+		return(g)
+	}
+	x = slideTree(g)
+	left_tree = x[[1]]
+	right_tree = x[[2]]
+	cutTree(left_tree)
+	cutTree(right_tree)
+	
+}
+
+
+
+cutTree(mst)
+
+
+
+
 
 
 
